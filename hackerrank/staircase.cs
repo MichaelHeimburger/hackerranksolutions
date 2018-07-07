fe2b9f113566f09b7040b544cdc6c3fd24a9780c
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-06 23:22:42 -0400

    another solved

--- hackerrank/staircase.cs
+++ hackerrank/staircase.cs
@@ -1,42 +1,36 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
 using System.Text;
-using System;
+using System.Threading.Tasks;
 
-class Solution
+namespace workspace
 {
-
-    // Complete the staircase function below.
-    static void staircase(int n)
+    class Program
     {
-        for (int i = 1; n > i; i++)
+        // Complete the staircase function below.
+        static void staircase(int n)
         {
-            for (int a=0; n - i > a; i++)
+            for (int i = 1; n >= i; i++)
             {
-                Console.Write(" ");
+                for (int a = 0; n - i > a; a++)
+                {
+                    Console.Write(" ");
+                }
+                for (int z = 0; i > z; z++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
             }
-            for(int z = 0; i > z ; z++)
-            {
-                Console.Write("#");
-            }
-            Console.WriteLine();
+
         }
 
-    }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
 
-    static void Main(string[] args)
-    {
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        staircase(n);
+            staircase(n);
+        }
     }
 }
