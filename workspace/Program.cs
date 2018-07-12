2d10a1921fbd9cd1667cceb20f79ad2109bec444
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-11 23:58:29 -0400

    retrying min max sum approach, completed pathtoprincess2

--- workspace/Program.cs
+++ workspace/Program.cs
@@ -19,23 +19,39 @@ class Solution
     static void miniMaxSum(int[] arr)
     {
         double sum = 0;
+        double maxnum = 0;
+        double minnum = 0;
         double summin = 0;
         double summax = 0;
 
+        foreach (int i in arr)
+        {
+            if
+}
+
         for (int i = 0; arr.Length > i; i++)
         {
-            sum = (arr[0] + arr[1] + arr[2] + arr[3] + arr[4]) - arr[i];
-
-            if ((sum > summax) || (i == 0))
+            if ((arr[i] < minnum) || (i == 0))
             {
-                summax = sum;
+                minnum = arr[i];
             }
-            if ((sum < summin) || (i == 0))
+            if (arr[i] > maxnum)
             {
-                summin = sum;
+                maxnum = arr[i];
             }
-
         }
+        foreach (int i in arr)
+        {
+            if (i != minnum)
+            {
+                summax += i;
+            }
+            if (i != maxnum)
+            {
+                summin += i;
+            }
+        }
+
         Console.WriteLine(summin + " " + summax);
     }
 
