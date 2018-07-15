08b8af0a403b6a62c10255bdb5baf9eef29e172a
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-14 22:13:13 -0400

    just solved all but one of the botcleans by writing one working code, neat!

---
+++ hackerrank/BOTCLEAN1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+
+    static void next_move(int posr, int posc, String[] board)
+    {
+        List<Trash> tlist = new List<Trash> { };
+        for (int i = 0; board.Length > i; i++)
+        {
+            for (int a = 0; board[i].Length > a; a++)
+            {
+                if (board[i][a] == 'd')
+                {
+                    Trash trash = new Trash();
+                    trash.row = i;
+                    trash.col = a;
+                    tlist.Add(trash);
+                }
+            }
+        } // trash locator
+        int choicerow = 0;
+        int choicecol = 0;
+        int choicedistance = 0;
+        int rowbuffer = 0;
+        int colbuffer = 0;
+        int totalbuffer = 0;
+        for (int i = 0; tlist.Count > i; i++)
+        {
+            rowbuffer = Math.Abs(tlist[i].row - posr);
+            colbuffer = Math.Abs(tlist[i].col - posc);
+            totalbuffer = rowbuffer + colbuffer;
+            if ((rowbuffer == 0) && (colbuffer == 0))
+            {
+                Console.WriteLine("CLEAN");
+                return;
+            }
+
+            if ((totalbuffer < choicedistance) || (i == 0))
+            {
+                choicecol = tlist[i].col;
+                choicerow = tlist[i].row;
+                choicedistance = Math.Abs(choicecol - posc) + Math.Abs(choicerow - posr);
+            }
+        } // distance selection
+
+
+        if ((choicerow == posr) && (choicecol == posc))
+        {
+            Console.WriteLine("CLEAN");
+            return;
+        }
+        if (choicerow > posr)
+        {
+            Console.WriteLine("DOWN");
+            return;
+        }
+        if (choicerow < posr)
+        {
+            Console.WriteLine("UP");
+            return;
+        }
+
+
+        if (choicecol > posc)
+        {
+            Console.WriteLine("RIGHT");
+            return;
+        }
+        if (choicecol < posc)
+        {
+            Console.WriteLine("LEFT");
+            return;
+        }
+
+    }
+
+    static void Main(String[] args)
+    {
+        String temp = Console.ReadLine();
+        String[] position = temp.Split(' ');
+        int[] pos = new int[2];
+        String[] board = new String[5];
+        for (int i = 0; i < 5; i++)
+        {
+            board[i] = Console.ReadLine();
+        }
+        for (int i = 0; i < 2; i++) pos[i] = Convert.ToInt32(position[i]);
+        next_move(pos[0], pos[1], board);
+    }
+}
+public class Trash
+{
+
+    public int row { get; set; }
+    public int col { get; set; }
+}
--- workspace/Program.cs
+++ workspace/Program.cs
@@ -1,64 +1,99 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
 
 class Solution
 {
 
-    // Complete the miniMaxSum function below.
-    static void miniMaxSum(int[] arr)
+    static void next_move(int posr, int posc, String[] board)
     {
-        double sum = 0;
-        double maxnum = 0;
-        double minnum = 0;
-        double summin = 0;
-        double summax = 0;
-
-        foreach (int i in arr)
+        List<Trash> tlist = new List<Trash> { };
+        for (int i = 0; board.Length > i; i++)
         {
-            if
+            for (int a = 0; board[i].Length > a; a++)
+            {
+                if (board[i][a] == 'd')
+                {
+                    Trash trash = new Trash();
+                    trash.row = i;
+                    trash.col = a;
+                    tlist.Add(trash);
+                }
+            }
+        } // trash locator
+        int choicerow = 0;
+        int choicecol = 0;
+        int choicedistance = 0;
+        int rowbuffer = 0;
+        int colbuffer = 0;
+        int totalbuffer = 0;
+        for (int i = 0; tlist.Count > i; i++)
+        {
+            rowbuffer = Math.Abs(tlist[i].row - posr);
+            colbuffer = Math.Abs(tlist[i].col - posc);
+            totalbuffer = rowbuffer + colbuffer;
+            if ((rowbuffer == 0) && (colbuffer == 0))
+            {
+                Console.WriteLine("CLEAN");
+                return;
+            }
+
+            if ((totalbuffer < choicedistance) || (i == 0))
+            {
+                choicecol = tlist[i].col;
+                choicerow = tlist[i].row;
+                choicedistance = Math.Abs(choicecol-posc) + Math.Abs(choicerow-posr);
+            }
+        } // distance selection
+
+
+        if ((choicerow == posr) && (choicecol == posc))
+        {
+            Console.WriteLine("CLEAN");
+            return;
+        }
+        if (choicerow > posr)
+        {
+            Console.WriteLine("DOWN");
+            return;
+        }
+        if (choicerow < posr)
+        {
+            Console.WriteLine("UP");
+            return;
+        }
+
+
+        if (choicecol > posc)
+        {
+            Console.WriteLine("RIGHT");
+            return;
+        }
+        if (choicecol < posc)
+        {
+            Console.WriteLine("LEFT");
+            return;
+        }
+
+    }
+
+    static void Main(String[] args)
+    {
+        String temp = Console.ReadLine();
+        String[] position = temp.Split(' ');
+        int[] pos = new int[2];
+        String[] board = new String[5];
+        for (int i = 0; i < 5; i++)
+        {
+            board[i] = Console.ReadLine();
+        }
+        for (int i = 0; i < 2; i++) pos[i] = Convert.ToInt32(position[i]);
+        next_move(pos[0], pos[1], board);
+    }
 }
+public class Trash
+{
 
-        for (int i = 0; arr.Length > i; i++)
-        {
-            if ((arr[i] < minnum) || (i == 0))
-            {
-                minnum = arr[i];
-            }
-            if (arr[i] > maxnum)
-            {
-                maxnum = arr[i];
-            }
-        }
-        foreach (int i in arr)
-        {
-            if (i != minnum)
-            {
-                summax += i;
-            }
-            if (i != maxnum)
-            {
-                summin += i;
-            }
-        }
-
-        Console.WriteLine(summin + " " + summax);
-    }
-
-    static void Main(string[] args)
-    {
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-        ;
-        miniMaxSum(arr);
-    }
+    public int row { get; set; }
+    public int col { get; set; }
 }
